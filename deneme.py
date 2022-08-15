47aba58306dfe8edbab957d7597e58106431bb38
Ömer Faruk Eren <101640251+omer832@users.noreply.github.com>
2022-08-15 20:31:37 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -21,10 +21,20 @@ def upload_file():
 	task_id = response.json()["data"]["task_ids"][0]
 	return(task_id)
 
-asd = upload_file()
 
-print(asd)
+
+def  asdasd(task_id):
+	
+	response = requests.post(api_url+'/filereport/'+str(task_id)+'/json/',)
+	
+	print(response.text)
+      
+task_id = upload_file()
+asdasd(task_id)
     
+    
+    
+#token problems   
 #curl -F file=@/path/to/file -F machine="cuckoo1" -H "Authorization: Token YOU_TOKEN" http://0.0.0.0:8000/apiv2/tasks/create/file/
 #dosya alma adresi
 #http://0.0.0.0:8000/filereport/20/json/   
