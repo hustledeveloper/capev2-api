dcf282fc44ebd6357f09d4867f406d704df20919
Ömer Faruk Eren <101640251+omer832@users.noreply.github.com>
2022-08-15 19:57:43 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,39 +1,29 @@
-import os
-import urllib.request
-from app import app
-from flask import Flask, request, redirect, jsonify
-from werkzeug.utils import secure_filename
+import json
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+api_url = 'http://0.0.0.0:8000'
 
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/file-upload', methods=['POST'])
 def upload_file():
-	# check if the post request has the file part
-	if 'file' not in request.files:
-		resp = jsonify({'message' : 'No file part in the request'})
-		resp.status_code = 400
-		return resp
-	file = request.files['file']
-	if file.filename == '':
-		resp = jsonify({'message' : 'No file selected for uploading'})
-		resp.status_code = 400
-		return resp
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		resp = jsonify({'message' : 'File successfully uploaded'})
-		resp.status_code = 201
-		return resp
-	else:
-		resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
-		resp.status_code = 400
-		return resp
+	multipart_data = MultipartEncoder(fields={'file': (
+							'danger.zip',
+							open('/home/omer/İndirilenler/danger.zip','rb'),
+							'application/octet-stream')})
 
-if __name__ == "__main__":
-    app.run()
+	header = {
+		'Content-Type': multipart_data.content_type
+		}
+	
+	response = requests.post(api_url+'/apiv2/tasks/create/file/',
+			  headers=header,
+			  data=multipart_data
+			)
+	task_id = response.json()["data"]["task_ids"][0]
+	return(task_id)
+
+asd = upload_file()
+
+print(asd)
     
 #curl -F file=@/path/to/file -F machine="cuckoo1" -H "Authorization: Token YOU_TOKEN" http://0.0.0.0:8000/apiv2/tasks/create/file/
 #dosya alma adresi
