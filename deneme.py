7cafd9cdf0ac47abd26409c001169fb1537ef727
Ömer Faruk Eren <101640251+omer832@users.noreply.github.com>
2022-08-16 17:17:24 +0300

    almost done
    need to wait for analysis

--- deneme.py
+++ deneme.py
@@ -11,7 +11,8 @@ def upload_file():
 							'application/octet-stream')})
 
 	header = {
-		'Content-Type': multipart_data.content_type
+		'Content-Type': multipart_data.content_type,
+		'Authorization': '6fc5be6ee1d904476ba6c255173ad3f086a6f537'
 		}
 	
 	response = requests.post(api_url+'/apiv2/tasks/create/file/',
@@ -23,41 +24,28 @@ def upload_file():
 
 
 
-def  asdasd(task_id):
-	token = {'csrftoken':'32waZaYf3EW8HNx9Nk2zoeTcYr0iqc2qlQChAGRp2nXd3FKu2dI72W0kbPcCwivs'}
-	res1 = requests.get(api_url+'/analysis/'+str(task_id)+'/')
-	response = requests.post(api_url+'/filereport/'+str(task_id)+'/json/', cookies=res1.cookies )
+def get_ceysin(task_id):
+
+	header = {
+		
+		'Authorization': '6fc5be6ee1d904476ba6c255173ad3f086a6f537'
+		}
+	
+	#token = {'csrftoken':'32waZaYf3EW8HNx9Nk2zoeTcYr0iqc2qlQChAGRp2nXd3FKu2dI72W0kbPcCwivs'}
+	#res1 = requests.get(api_url+'/analysis/'+str(task_id)+'/')
+	response = requests.get(api_url+'/filereport/'+str(task_id)+'/json/', headers=header)
 	
 	print(response.text)
       
 task_id = upload_file()
-asdasd(task_id)
-    
-    
-    
-  
+get_ceysin('29')
+print(task_id)
+ 
+""" 
+daha önceden analiz edilmiş bir işlemin id'si ile json dosyasını almayı başardım, şimdi tek sorun
+yapılan bir analiz için, json sonucu dönmesine kadar geçecek 2 dakikada bekleyebilecek ve sonuç çıkınca sonucu alacak bir kod yazmak, şu an kod analiz yaptığı an sonucu istiyor ve çıkmamış sonucu alamayıp hata veriyor 
+""" 
+ 
 #curl -F file=@/path/to/file -F machine="cuckoo1" -H "Authorization: Token YOU_TOKEN" http://0.0.0.0:8000/apiv2/tasks/create/file/
-
 #http://0.0.0.0:8000/filereport/20/json/  
 
-
-# cookie: csrftoken=DGDrHNLkvs9Mrymby1wjlFUzl1XW8oQny5Ux56lpvfjB2Mexm6QYm67N6x5TKlmg
-#Error:  You are seeing this message because this site requires a CSRF cookie when submitting forms. This cookie is required for security reasons, to ensure that your browser is not being hijacked by third parties.
-"""
-Help
-Reason given for failure:
-
-    CSRF token missing.
-    
-In general, this can occur when there is a genuine Cross Site Request Forgery, or when Djangoâs CSRF mechanism has not been used correctly. For POST forms, you need to ensure:
-
-Your browser is accepting cookies.
-The view function passes a request to the templateâs render method.
-In the template, there is a {% csrf_token %} template tag inside each POST form that targets an internal URL.
-If you are not using CsrfViewMiddleware, then you must use csrf_protect on any views that use the csrf_token template tag, as well as those that accept the POST data.
-The form has a valid CSRF token. After logging in in another browser tab or hitting the back button after a login, you may need to reload the page with the form, because the token is rotated after a login.
-Youâre seeing the help section of this page because you have DEBUG = True in your Django settings file. Change that to False, and only the initial error message will be displayed.
-
-You can customize this page using the CSRF_FAILURE_VIEW setting.
-
-"""
